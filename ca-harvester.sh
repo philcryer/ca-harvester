826a1d55def0bb6f9246c8a06f537dc0ba7c5cfa
fak3r <fak3r@fak3r.com>
2012-02-15 20:12:28 -0600

    updated readme section

--- ca-harvester.sh
+++ ca-harvester.sh
@@ -2,26 +2,32 @@
 #
 ###############
 # Distributed under the terms of the BSD License.
-# Copyright (c) 2011 Phil Cryer phil@philcryer.com
+# Copyright (c) 2012 Phil Cryer phil@philcryer.com
 # Source https://github.com/philcryer/ca-harvester
 ###############
 #
 # name:		ca-harvester.sh
 #
-# features:	randomly creates a C l o u d A p p short URL (ie http://cl . ly/xxxx)
+# features:	creates a random  C l o u d A p p  short URL (cl . ly/xxxx) 
 #		checks that URL, if it finds content it downloads it
 #		renames the file, preserving the url suffix in the filename
 #		limits overall size of download in MB (default 100MB)
 #
-# disclosure:	discussed this vulnerabilityDate: Thu, 1 Dec 2011 23:09:20 -0600
+# usage:	chmod +x ca-havester.sh
+#		./ca-harvester.sh
+#		(optional) edit ca-harvester.sh and modify SIZE_LIMIT
 #
-# license:	this is open source software released under the Simplified BSD License
-#       	http://www.opensource.org/licenses/bsd-license.php
+# disclosure:	I disclosed this vulnerability to the company that runs this 
+#		service on Thursday, 1 Dec 2011 at 23:09:20 -0600  The code
+#		was released Wednesday, 15 February 2012 at 20:09:15 -0600
 #
-# inspiration:	@dcurtis C l o u d A p p  R o u l e t t e;
-#		http://cargo.dustincurtis.com.s3.amazonaws.com/c l o u d a p p-roulette.html 
-#		C l o u d A p p banned that webapp, but did nothing to for users' privacy,
-#		my goal with this project was to prove that. 
+# license:	this is open source software released under the Simplified BSD
+#		License (http://www.opensource.org/licenses/bsd-license.php)
+#
+# inspiration:	@dcurtis http://cargo.dustincurtis.com.s3.amazonaws.com/
+#		c l o u d a p p - rou let te.html 
+#		C l o u d A p p  banned that webapp, but did nothing more for 
+#		users' privacy. The goal of this project was to prove that. 
 #
 # ran_quote:	there were more but now drops
 #
